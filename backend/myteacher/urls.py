23b70f568c8994b9247e59565b903f740b511484
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2022-10-19 11:40:31 -0300

    :sparkles: urls

--- backend/myteacher/urls.py
+++ backend/myteacher/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from teacher.views import ProfessorAPIView, CadastrarAulaAPiView
+from teacher.views import ProfessorAPIView, CadastrarAulaAPiView, RegisterView, LoginView, UserView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('professores/', ProfessorAPIView.as_view()),
     path('professores/<int:id>/aulas', CadastrarAulaAPiView.as_view()), 
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('user/', UserView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]

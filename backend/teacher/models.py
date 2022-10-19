49286ef36bbfdcc90f698e28037072bafd4a5c3e
Sabrina Naderer <95416508+sanaderer@users.noreply.github.com>
2022-10-19 08:55:59 -0300

    models user

--- backend/teacher/models.py
+++ backend/teacher/models.py
@@ -10,4 +10,18 @@ class Aula(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False)
     professor = models.ForeignKey(to=Professor, on_delete=models.CASCADE, related_name="aulas", null=False, blank=False)
-    
+
+class User(models.Model):
+    username = models.CharField(db_index=True, max_length=255, unique=True, default="")
+    email = models.EmailField(db_index=True, unique=True, null=True, blank=True, default="")
+    firstname = models.CharField(max_length=150, blank=True, default="")
+    lastname = models.CharField(max_length=150, blank=True, default="")
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f"{self.email}"
--- backend/teacher/serializers.py
+++ backend/teacher/serializers.py
@@ -22,3 +22,19 @@ class AulaSerializer(serializers.ModelSerializer):
         model = Aula
         fields = '__all__'
 
+class UserSerializer(serializers.ModelSerializer):
+    class User(AbstractUser):
+        username = models.CharField(db_index=True, max_length=255, unique=True, default="")
+        email = models.EmailField(db_index=True, unique=True, null=True, blank=True, default="")
+        firstname = models.CharField(max_length=150, blank=True, default="")
+        lastname = models.CharField(max_length=150, blank=True, default="")
+        is_active = models.BooleanField(default=True)
+        is_staff = models.BooleanField(default=False)
+        created = models.DateTimeField(default=now)
+        updated = models.DateTimeField(auto_now=True)
+
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = ['username']
+
+        def __str__(self):
+            return f"{self.email}"

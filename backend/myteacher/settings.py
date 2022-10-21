b6f60f9ee107fda2fe28d719fcedec1ec3e2d7ec
Selamanse <selamanse@scheinfrei.info>
2022-10-22 00:09:38 +0200

    Improve sqlite db path building

--- backend/myteacher/settings.py
+++ backend/myteacher/settings.py
@@ -1,6 +1,6 @@
-from pathlib import Path
+from os.path import dirname, abspath
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = dirname(dirname(abspath(__file__)))
 
 SECRET_KEY = 'django-insecure-s!fy)lr0fmhpvab(tbu9eglkfsp0(+ebrp%c_d9qwcb_furq2d'
 
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'myteacher.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
     }
 }
 
